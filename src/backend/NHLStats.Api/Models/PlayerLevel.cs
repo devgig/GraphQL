310da55cb4236189fc47f65b6a20c798557b022e
LAPTOP-8MDRP3R0\geoff <geoff.niehaus@hotmail.com>
2019-11-14 18:51:07 -0600

    update

---
+++ src/backend/NHLStats.Api/Models/PlayerLevel.cs
@@ -0,0 +1,21 @@
+﻿using GraphQL.Types;
+using NHLStats.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NHLStats.Api.Models
+{
+    public class PlayerLevelEnumType : EnumerationGraphType<PlayerLevel>
+    {
+        public PlayerLevelEnumType()
+        {
+            Name = "Level";
+            Description = "The players professional level";
+
+        }
+    }
+
+   
+}
---
+++ src/backend/NHLStats.Core/Models/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NHLStats.Core.Models
+{
+    public enum PlayerLevel
+    {
+        Pro,
+        SemiPro,
+        Amature
+    }
+}
